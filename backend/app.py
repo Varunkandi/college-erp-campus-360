7530601484055ecc3961e8d54be093ef91a590da
Varunkandi <135459124+Varunkandi@users.noreply.github.com>
2026-03-23 00:55:24 +0530

    final cors fix

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import sqlite3 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 # ======================
