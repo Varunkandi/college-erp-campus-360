7695e2cf7b62398b34a91ff43a5b044ea03832df
Varunkandi <135459124+Varunkandi@users.noreply.github.com>
2026-03-23 01:04:29 +0530

    final cors fix

--- backend/app.py
+++ backend/app.py
@@ -874,6 +874,8 @@ with get_db() as db:
 # RUN SERVER
 # ======================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
     
